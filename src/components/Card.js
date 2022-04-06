8df913228ae63cd26d30ae494ee3dbc7a67975ff
Hamed Bahrami <hamedbahrami@Air-von-Hamed.fritz.box>
2022-04-06 10:01:57 +0200

    Issue 3, task2commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,24 @@ import React from "react";
 const Card = () => {
   return (
     <div>
+    <CardItem>
       <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Hier muss ein Bild sein" />
       <h2> Morty Smith </h2>
+    </CardItem>
     </div>
   );
 };
+
+
+const CardItem = styled.li`
+
+display: flex;
+align-items: center;
+border: 3px sold black;
+margin: 1em;
+list-style-type: none;
+`;
+
+
 
 export default Card;
